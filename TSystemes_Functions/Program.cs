923574679982f14fe45646161e38e560205d90f2
Nikitamagar855 <89832430+Nikitamagar855@users.noreply.github.com>
2022-03-24 11:28:55 +0530

    Add project files.

---
+++ TSystemes_Functions/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystemes_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithmaticOperation objAO = new ArithmaticOperation();
+            objAO.Addition();
+            objAO.substraction();
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+
+
+    public class ArithmaticOperation
+    {
+        public void Addition()
+        {
+            int x = 69;
+            int y = 88;
+            int sum = x + y;
+            Console.WriteLine($"Addition is{sum}");
+
+
+        }
+        public void substraction()
+        {
+            int a = 55;
+            int b = 44;
+            int sub = a - b;
+            Console.WriteLine($"Substraction is {sub}");
+        }
+        public void multiplication()
+        {
+            int a = 55;
+            int b = 44;
+            int mult = a * b;
+            Console.WriteLine($"Multiplication is {mult}");
+        }
+        public void Division()
+        {
+            int a = 55;
+            int b = 44;
+            int div = a / b;
+            Console.WriteLine($"Division is {div}");
+        }
+
+
+
+    }
+
+
+}
